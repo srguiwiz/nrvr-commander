e676e9e1619cf051c938a5ac9bf272322a599a2b
Leo Baschy <srguiwiz12@nrvr.com>
2013-07-14 12:33:30 -0700

    Improve commandToChangeStaticIPAddress

--- src/nrvr/el/clone.py
+++ src/nrvr/el/clone.py
@@ -22,21 +22,30 @@ class Clone():
     """Utilities for manipulating a Enterprise Linux machines for cloning."""
 
     @classmethod
-    def commandToChangeStaticIPAddress(cls, oldIpAddress, newIpAddress, interface="eth0"):
+    def commandToChangeStaticIPAddress(cls, oldIpAddress, newIpAddress, interface=None):
         """Build command to change static IP address.
         
         Must be root to succeed.
         
         As implemented works in Enterprise Linux versions 6.x.
         
+        interface
+            a string, e.g. "eth0".
+        
         Return command to change static IP address."""
         oldIpAddress = IPAddress.asString(oldIpAddress)
         newIpAddress = IPAddress.asString(newIpAddress)
+        # quite sensitive to quoting and not quoting
+        command = r"sed -i -e 's/=\"\?" + re.escape(oldIpAddress) + r"\"\?/=\"" + re.escape(newIpAddress) + r"\"/'"
+        if interface:
+            command += r" '/etc/sysconfig/network-scripts/ifcfg-" + re.escape(interface) + r"'"
+        else:
+            # quite sensitive to quoting and not quoting
+            command = r"for f in /etc/sysconfig/network-scripts/ifcfg-* ; do " + command + r" $f ; done"
         # oddly has been observed to require two times service network restart
-        command = r"sed -i -e 's/" + re.escape(oldIpAddress) + r"/" + re.escape(newIpAddress) + \
-                  r"/' '/etc/sysconfig/network-scripts/ifcfg-" + re.escape(interface) + \
-                  r"' ; ( service network restart ; service network restart ) &"
+        command += r" ; nohup sh -c 'service network restart ; service network restart' &> /dev/null &"
         return command
 
 if __name__ == "__main__":
     print Clone.commandToChangeStaticIPAddress("10.123.45.67", "10.123.45.68")
+    print Clone.commandToChangeStaticIPAddress("10.123.45.67", "10.123.45.68", interface="eth0")
