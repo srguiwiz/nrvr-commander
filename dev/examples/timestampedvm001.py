4490cd814aa8692e57393cf5b118e95482de9fc6
Leo Baschy <srguiwiz12@nrvr.com>
2013-08-22 15:15:14 -0700

    Nitpick

--- dev/examples/timestampedvm001.py
+++ dev/examples/timestampedvm001.py
@@ -13,17 +13,14 @@ from nrvr.util.requirements import SystemRequirements
 from nrvr.util.times import Timestamp
 from nrvr.vm.vmware import VMwareHypervisor
 
-# TODO document this is a good way to preflight check
+# this is a good way to preflight check
 SystemRequirements.commandsRequiredByImplementations([VMwareHypervisor],
                                                      verbose=True)
-# TODO document this is a good way to preflight check
+# this is a good way to preflight check
 VMwareHypervisor.localRequired()
 
-# BEGIN not really necessary, just showing how to
-# TODO document this way of making new VM names and directories
+# demoing one way of making new VM names and directories,
 # timestamp to the microsecond should be good enough
 exampleName = "example" + Timestamp.microsecondTimestamp()
 exampleVmxFilePath = os.path.join(VMwareHypervisor.local.suggestedDirectory, exampleName, exampleName + ".vmx")
 print exampleVmxFilePath
-# END not really necessary, just showing how to
-
