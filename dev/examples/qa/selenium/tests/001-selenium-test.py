fac746ba4f82fda56b497e26120e359989024957
Leo Baschy <srguiwiz12@nrvr.com>
2013-09-25 14:10:03 -0700

    Improve demonstration script

--- dev/examples/qa/selenium/tests/001-selenium-test.py
+++ dev/examples/qa/selenium/tests/001-selenium-test.py
@@ -8,33 +8,51 @@ import time
 print "STARTING running %s" % (__file__)
 sys.stdout.flush()
 
+hasReachedEnd = False
 try:
     browser = webdriver.Firefox()
     browser.implicitly_wait(45)
     
-    browser.get("http://www.bbc.co.uk/")
-    
-    # this is just a website we had picked as an example,
-    # it may have different links by the time you are trying it out
-    #
-    # why this doesn't go all of below links all the time apparently is a Selenium issue,
-    # possibly specific to versions of Selenium,
-    # maybe to a browser specific driver
-    #
-    # you should figure that out for your own site you are testing, and for your own script
-    #
-    # a universally correctly running example will gladly be included here as a replacement
-    
-    browser.find_element_by_link_text("News").click()
-    browser.find_element_by_link_text("US & Canada").click()
-    browser.find_element_by_link_text("Europe").click()
-    browser.find_element_by_link_text("Weather").click()
-    browser.find_element_by_link_text("Capital").click()
-    
-    time.sleep(3)
-    browser.quit()
+    try:
+        browser.get("http://www.bbc.co.uk/")
+        
+        # this is just a website we had picked as an example,
+        # it may have different links by the time you are trying it out
+        #
+        # why this doesn't go all of below links all the time apparently is a Selenium issue,
+        # possibly specific to versions of Selenium,
+        # maybe to a browser specific driver
+        #
+        # you should figure that out for your own site you are testing, and for your own script
+        #
+        # a universally correctly running example will gladly be included here as a replacement
+        
+        time.sleep(3)
+        browser.find_element_by_link_text("News").click()
+        time.sleep(3)
+        browser.find_element_by_link_text("US & Canada").click()
+        time.sleep(3)
+        browser.find_element_by_link_text("Europe").click()
+        time.sleep(3)
+        browser.find_element_by_link_text("Weather").click()
+        time.sleep(3)
+        browser.find_element_by_link_text("Capital").click()
+        
+        hasReachedEnd = True
+    finally:
+        time.sleep(3)
+        browser.quit()
+
+except Exception as ex:
+    sys.stderr.flush()
+    print "EXCEPTION running %s:" % (__file__)
+    print ex
+    sys.stdout.flush()
+    hasReachedEnd = True
 
 finally:
     sys.stderr.flush()
+    if not hasReachedEnd:
+        print "ABNORMAL unspecified end to running %s" % (__file__)
     print "DONE running %s" % (__file__)
     sys.stdout.flush()
