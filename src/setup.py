07296f73d0881926cfdd45049cfa81264fb22c97
Leo Baschy <srguiwiz12@nrvr.com>
2014-01-02 07:12:43 -0800

    Declare 1.5.0

--- src/setup.py
+++ src/setup.py
@@ -3,7 +3,7 @@
 from distutils.core import setup
 
 setup(name="NrvrCommander",
-      version="1.4.9",
+      version="1.5.0",
       description="Tools for automation.",
       long_description="""Tools for automation.
       
